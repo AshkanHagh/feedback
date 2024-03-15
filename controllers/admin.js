5b8a914a52149bc7311b84eab7d045e39705256b
Ashkan <AshkanHagh01@gmail.com>
2024-03-15 17:05:48 +0330

    admin panel

---
+++ controllers/admin.js
@@ -0,0 +1,83 @@
+const Message = require('../models/message');
+const User = require('../models/user');
+
+
+exports.getMessages = async (req, res, next) => {
+
+    try {
+        const message = await Message.find().populate('username', 'username');
+        if(!message) {
+
+            const error = new Error('Nothing found');
+            error.statusCode = 404;
+            throw error;
+        }
+
+        res.status(200).json({message : 'messages fethced', messages : message});
+
+    } catch (error) {
+        
+        if(!error.statusCode) {
+
+            error.statusCode = 500;
+        }
+        next(error);
+    }
+
+}
+
+exports.getSingleMessage = async (req, res, next) => {
+
+    try {
+        const message = await Message.findById(req.params.id).populate('username', 'username');
+        if(!message) {
+            
+            const error = new Error('nnothing found');
+            error.statusCode = 404;
+            throw error;
+        }
+
+        res.status(200).json({message : 'User message fetched', msg : message});
+
+    } catch (error) {
+        
+        if(!error.statusCode) {
+
+            error.statusCode = 500;
+        }
+        next(error);
+    }
+
+}
+
+exports.deleteMessage = async (req, res, next) => {
+
+    try {
+        const message = await Message.findById(req.params.id);
+        if(!message) {
+            
+            const error = new Error('nnothing found');
+            error.statusCode = 404;
+            throw error;
+        }
+
+        await message.deleteOne();
+
+        const user = await User.findById(req.userId);
+
+        user.messages.remove(req.params.id);
+
+        await user.save();
+
+        res.status(200).json({message : 'Message has been deleted', messageId : message._id});
+
+    } catch (error) {
+        
+        if(!error.statusCode) {
+
+            error.statusCode = 500;
+        }
+        next(error);
+    }
+
+}
---
+++ routes/admin.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const isAuth = require('../middlewares/verify-token');
+
+const adminControl = require('../controllers/admin');
+
+
+router.get('/', adminControl.getMessages);
+
+router.get('/:id', adminControl.getSingleMessage);
+
+router.delete('/:id', isAuth, adminControl.deleteMessage);
+
+
+module.exports = router;
