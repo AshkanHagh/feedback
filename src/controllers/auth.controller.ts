bc7ef575689ffceed9b902595d296e2442177f1d
Ashkan <AshkanHagh01@gmail.com>
2024-08-27 08:30:45 +0330

    refactor(auth): refine if statements and improve return types

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,16 +2,17 @@ import type { Context } from 'hono';
 import { CatchAsyncError, decodeToken, sendToken, type ConditionResponse } from '../utils';
 import type { SocialAuth } from '../schemas';
 import prisma from '../configs/prisma.config';
-import type { Users } from '@prisma/client';
+import type { User } from '@prisma/client';
 import { hgetall } from '../cache';
 import { getCookie } from 'hono/cookie';
 
 export const socialAuth = CatchAsyncError(async (context : Context) => {
     const { email, name, image } = context.req.validationData.json as SocialAuth;
-    const emailSearchCache : Users = await hgetall<Users>(`user:${email}`, 604800);
+    const emailSearchCache : User = await hgetall<User>(`user:${email}`, 604800);
 
-    const isEmailExists : Users | null = emailSearchCache ? emailSearchCache : await prisma.users.findUnique({where : {email}});
-    const userDetail : Users = isEmailExists ? isEmailExists : await prisma.users.create({data : {email, name, image}})
+    const isEmailExists : User | null = emailSearchCache && Object.keys(emailSearchCache).length 
+    ? emailSearchCache : await prisma.user.findUnique({where : {email}});
+    const userDetail : User = isEmailExists ? isEmailExists : await prisma.user.create({data : {email, name, image}})
 
     const { accessToken, user } : ConditionResponse = await sendToken(userDetail, context, 'register');
     return context.json({success : true, userDetail : user, accessToken});
@@ -19,8 +20,8 @@ export const socialAuth = CatchAsyncError(async (context : Context) => {
 
 export const refreshToken = CatchAsyncError(async (context : Context) => {
     const refresh_token : string | undefined = getCookie(context, 'refresh_token');
-    const decodedUser : Users = decodeToken(refresh_token ?? '', process.env.REFRESH_TOKEN);
-    const userDetail : Users = await hgetall(`user:${decodedUser.id}`, 604800);
+    const decodedUser : User = decodeToken(refresh_token ?? '', process.env.REFRESH_TOKEN);
+    const userDetail : User = await hgetall(`user:${decodedUser.id}`, 604800);
 
     const accessToken : string = await sendToken(userDetail, context, 'refresh');
     return context.json({success : true, accessToken});
